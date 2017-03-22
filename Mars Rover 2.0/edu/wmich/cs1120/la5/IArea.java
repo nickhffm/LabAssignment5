4b49bbd3793d17dc0cd8dbe9f8a4604fe945ae12
Macallister Armstrong <macallister.j.armstrong@wmich.edu>
2017-03-22 12:32:15 -0400

    Added Area interface and classes. Identical to LA3/ not finished.

--- Mars Rover 2.0/edu/wmich/cs1120/la5/IArea.java
+++ Mars Rover 2.0/edu/wmich/cs1120/la5/IArea.java
@@ -1,5 +1,47 @@
 package edu.wmich.cs1120.la5;
 
+/**
+ * Interface for the Area class.
+ * @author Macallister Armstrong
+ */
 public interface IArea {
-
+	
+	/**
+	 * Returns basicEnergyCost.
+	 * @return basicEnergyCost	 */
+	double getBasicEnergyCost();
+	/**
+	 * Sets basicEnergyCost.
+	 * @param basicEnergyCost
+	 */
+	void setBasicEnergyCost(double basicEnergyCost);
+	
+	/**
+	 * Returns elevation.
+	 * @return elevation
+	 */
+	double getElevation(); 
+	/**
+	 * Sets elevation.
+	 * @param elevation
+	 */
+	void setElevation(double elevation);
+	
+	/**
+	 * Returns radiation.
+	 * @return radiation
+	 */
+	double getRadiation();
+	/**
+	 * Sets radiation.
+	 * @param radiation
+	 */
+	void setRadiation(double radiation);
+	
+	/**
+	 * Calculates energy consumed in a location.
+	 * @return energy_cost
+	 */
+	double calcConsumedEnergy();
 }
+

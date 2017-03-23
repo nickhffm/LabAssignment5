834771d025c5e8ff587e93b585fc2075b9895899
Nick Huffman <nickhffm@nick.d.huffman@wmich.edu>
2017-03-23 15:23:54 -0400

    Fixed GUI errors

--- Mars Rover 2.0/edu/wmich/cs1120/la5/EnergyAnalyzer.java
+++ Mars Rover 2.0/edu/wmich/cs1120/la5/EnergyAnalyzer.java
@@ -1,5 +1,55 @@
 package edu.wmich.cs1120.la5;
 
-public class EnergyAnalyzer {
+import java.util.ArrayList;
+
+public class EnergyAnalyzer implements IRover{
+
+	@Override
+	public IMapCreator getMapCreator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMapCreator(IMapCreator mc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<IArea> getPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPath(int startRow, int startCol) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void analyzePath() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getAnalysis() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setAnalysis() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double calculateEnergy() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- Mars Rover 2.0/edu/wmich/cs1120/la5/LA5Main.java
+++ Mars Rover 2.0/edu/wmich/cs1120/la5/LA5Main.java
@@ -9,18 +9,5 @@ public class LA5Main {
 
 	public static void main(String[] args) {
 		MainWindow.startGUI();
-		IRover rover = new Rover();
-		IMapCreator mc = new MapCreatorFromTxt();
-		rover.setMapCreator(mc);
-		TerrainScanner ts = new TerrainScanner();
-		mc.setScanner(ts);
-		setTerrain(mc.getTerrain());
-		try {
-			mc.scanTerrain("terrain.txt", 8);
-		} catch (FileNotFoundException e){
-			System.out.println(e.getMessage());
-		}
 	}
-	
-	public static void setTerrain(IArea[][] pTerrain) {terrain = pTerrain;}
 }
